b981952dc24ec5cea6d475142210facb733fdfe3
zhaosheng <zs17337825170@163.com>
2022-01-17 16:42:20 +0800

    fix cookie

--- pages/api/top_song.js
+++ pages/api/top_song.js
@@ -1,9 +1,6 @@
 // 新歌速递
 import { top_song } from 'NeteaseCloudMusicApi'
 
-export default async function handler(
-  req,
-  res
-) {
+export default async function handler(req, res) {
   res.status(200).json(await top_song({ type: 0 }))
 }
